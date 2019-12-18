086e0ed489a4b4180517acae8717275d69107aeb
tima <florovtf@gmail.com>
2019-12-18 12:31:48 +0300

    1 задание

--- react-intro-master/app.js
+++ react-intro-master/app.js
@@ -2,20 +2,5 @@ var image=React.createElement('img',{src:'https://png.cmtt.space/paper-media/6e/
 var title=React.createElement('h1',null,'привет реакт')//добавляем заголовок 
 var subtitle=React.createElement('p',null,'что-то тут лежит')//добавляем текс под ним 
 var container =React.createElement('div',{className:'container'},image,title,subtitle);//"контейнер" куда мы складываем все наши элементы 
-/*function Hero(props) {
-    return (
-        React.createElement('div', { className: 'container' }, 
-            React.createElement('img', { src: props.imageUrl }),
-            React.createElement('h1', null, props.title),
-            React.createElement('p', null, props.subtitle)
-        )
-    );
-}
-
-var hero = React.createElement(Hero, { title: 'React',
-                                       subtitle: 'Библиотека для создания пользовательских интерфейсов',
-                                       imageUrl: 'https://facebook.github.io/react/img/logo.svg' });
-*/
-//vmjkvnkrlvnslvs
 
 ReactDOM.render(container, document.getElementById('root'));//передаем значение по id
