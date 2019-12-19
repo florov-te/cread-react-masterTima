1340837e2b3128dcae65584e778f8744aa9162f7
tima <florovtf@gmail.com>
2019-12-19 14:37:16 +0300

    задание 7.1

--- react-intro-master/app.jsx
+++ react-intro-master/app.jsx
@@ -1,9 +1,17 @@
 function Hero(props){
+    //добавляем переменную "count"
+    let count=0;
+    //создаем функцию для картинки  
+    function clickClack(){
+       count+=1;
+       console.log(count)
+    }
     return(
         <div className='container'>
             <h1>{props.title}</h1>
             <p>{props.subtitle} </p>
-            <img src={props.urlImage}/>
+            <img src={props.urlImage} onClick={clickClack}/> 
+            <div className='count'>{count}</div>
         </div>
     );
 }
