8a7675e384493a62ef5d284016127978d1f7ce2e
tima <florovtf@gmail.com>
2019-12-19 14:52:50 +0300

    задание 7.2

--- react-intro-master/app.jsx
+++ react-intro-master/app.jsx
@@ -1,22 +1,29 @@
-function Hero(props){
-    //добавляем переменную "count"
-    let count=0;
-    //создаем функцию для картинки  
-    function clickClack(){
-       count+=1;
-       console.log(count)
+
+var Hero= React.createClass({
+    getInitialState:function(){
+        return{
+            count: 0
+        }
+    },
+    clickClack:function(){
+        this.state.count+=1;
+        console.log(this.state.count);
+    },
+    render:function(){
+        return(
+            <div className='container'>
+                <div className='count'>{this.state.count}</div>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.subtitle} </p>
+                <img src={this.props.urlImage} onClick={this.clickClack}/>
+            </div>
+        );
     }
-    return(
-        <div className='container'>
-            <h1>{props.title}</h1>
-            <p>{props.subtitle} </p>
-            <img src={props.urlImage} onClick={clickClack}/> 
-            <div className='count'>{count}</div>
-        </div>
-    );
-}
-ReactDOM.render(<Hero title='React'
-                      subtitle='что тут должно быть'
-                      urlImage='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png'/>
+});
+ReactDOM.render(<div>
+                     <Hero title='React'
+                     subtitle='что тут должно быть'
+                     urlImage='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png'/>
+                </div>
                       
 ,document.getElementById('root'));
